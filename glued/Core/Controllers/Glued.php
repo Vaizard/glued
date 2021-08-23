2e945de781a9cdb4ec7b9d7d7753d39330c10652
Pavel Stratil <pavel@vaizard.org>
2021-08-23 23:48:54 +0000

    hell yeah

--- glued/Core/Controllers/Glued.php
+++ glued/Core/Controllers/Glued.php
@@ -22,7 +22,27 @@ class Glued extends AbstractTwigController
     public function __invoke(Request $request, Response $response, array $args = []): Response
     {
 
+
+
 //INSERT INTO t VALUES(UUID_TO_BIN(UUID(), true));”
+// Add data to tokens
+// https://id.industra.space/auth/admin/master/console/#/realms/t1/clients/75cdbe25-7c19-4fba-8a70-ddf629011d39/add-mappers
+// Add metadata to registration
+// https://keycloakthemes.com/blog/how-to-add-custom-field-keycloak-registration-page
+// https://github.com/keycloak/keycloak/tree/master/examples/themes
+// actions
+// https://id.industra.space/auth/admin/master/console/#/realms/t1/clients/75cdbe25-7c19-4fba-8a70-ddf629011d39/authz/resource-server/scope
+// resources
+// https://id.industra.space/auth/admin/master/console/#/realms/t1/clients/75cdbe25-7c19-4fba-8a70-ddf629011d39/authz/resource-server/resource
+// https://id.industra.space/auth/admin/master/console/#/realms/t1/clients/75cdbe25-7c19-4fba-8a70-ddf629011d39/authz/resource-server/resource
+// policies assign users/roles to actions and resources.
+// assigning a user to a tenant/role tuple could be done by
+// - assigning a user to group
+// - defining first-level group as role
+// - defining 2nd and nth level groups as groups (and subgroups)
+// 
+// 
+
 
         return $this->render($response, 'Core/Views/glued.twig', [
 /*                'certs' => $certs,
--- glued/Core/Middleware/AuthorizationMiddleware.php
+++ glued/Core/Middleware/AuthorizationMiddleware.php
@@ -73,66 +73,7 @@ final class AuthorizationMiddleware extends AbstractMiddleware implements Middle
     {
         // Set global variables used everywhere
         $this->merge_authn($request);
-        $e = null;
 
-/*
-        if ($this->auth->check() === true) {
-            $user_authn = $this->auth->fetch();
-            if ($user_authn) {
-                $GLOBALS['_GLUED']['authn']['success'] = true;
-                $GLOBALS['_GLUED']['authn']['object']  = $user_authn;
-                $user_authn['success'] = true;
-                $this->view->getEnvironment()->addGlobal('authn', $user_authn);
-                $e = $this->enforcer;
-            }
-        }
-
-        // TODO skip this for api paths $this->settings->auth->jwt->path
-        $this->view->getEnvironment()->addFunction(new TwigFunction('enforce', function ($obj, $dom = "0", $sub = null, $act = "r") use ($e) {
-            if (is_null($e)) return false;
-            $sub = $GLOBALS['_GLUED']['authn']['user_id'] ?? null;
-            $m = $e->getModel();
-            $r = $e->enforce((string)$sub, (string)$dom, (string)$obj, (string)$act); 
-            return $r;
-        }));
-
-        if ($this->auth->check() === true) {
-            // AUTHORIZATION
-            // TODO Uncommenting the stuff below is viable once problems with casbin are resolved
-
-            //$e = $this->enforcer;
-            //$m = $e->getModel();
-            //getPolicy(string $sec, string $ptype):
-            //print_r($m->getPolicy('g','g'));
-            //print_r($m->getFilteredPolicy('g','g',1,'admin'));
-            //die();
-            //$f = $m::loadFunctionMap(); // ok
-            //$f = $e->getRoleManager(); // ok
-            //$f = $e->getPolicy();
-            //$f = $e->getFilteredPolicy(0, '1'); 
-            //$f = $e->getFilteredPolicy(1, '0');
-            //$f = $e->getFilteredPolicy(2, '/ui/stor');
-            //$f = $m->getFilteredPolicy('p','p',2, '/ui/stor');
-            //$f = $e->getFilteredGroupingPolicy(0, '1');
-            //$f = $e->getRolesForUser('2');
-            //$f = $e->getRolesForUser('2');
-            // print("<pre>".print_r($f,true)."</pre>");
-
-            // TODO: we should support auth_id in the enforcing so that users can fine tune access control for different credentials
-            //$sub = $GLOBALS['_GLUED']['authn']['user_id']; // the user that wants to access a resource.
-            //$obj = "data1"; // the resource that is going to be accessed.
-            //$act = "read"; // the operation that the user performs on the resource.
-            //$e->enforce($u, '0', 'add-correct-route-here', 'r');  
-
-            // $e->name, or $m->name?
-            //print_r( $m->getPolicy(1,1,'all','read') ) ;
-            //$e->addRoleForUser('alice', 'admin'); 
-            //$e->addPermissionForUser('member', '/foo', 'GET');
-            //$e->addPolicy('eve', 'data3', 'read');
-            //$e->getRolesForUser('alice');
-
-        }
-    */
 
         
         $oidc_conf_uri = 'https://id.industra.space/auth/realms/t1/.well-known/openid-configuration'; // todo lower case!
@@ -200,12 +141,30 @@ final class AuthorizationMiddleware extends AbstractMiddleware implements Middle
             $this->view->getEnvironment()->addGlobal('ahdr', $accesstoken);
             $this->view->getEnvironment()->addGlobal('jwt_claims', $jwt->claims->all() ?? []);
             $this->view->getEnvironment()->addGlobal('jwt_header', $jwt->header->all() ?? []);        
+
+            // TODO check validify of sub, email, etc.
+            // TODO create profile and set attrs
+            // TODO handle race conditions and db errors.
+
+            $data = [
+               "c_uuid" => $this->db->func( 'uuid_to_bin(?, true)', [ $jwt->claims->all()['sub'] ] ),
+               "c_email" => $jwt->claims->all()['email'],
+               "c_nick" => $jwt->claims->all()['preferred_username']
+            ];
+            if ($this->db->insert('t_core_users', $data)) $respond = 'Welcome';
+            else $respond = 'Hello again';
+            $this->view->getEnvironment()->addGlobal('core_users', $respond);        
+
+
         } catch (\Exception $e) {
                 if ($request->getUri()->getPath() != $this->routecollector->getRouteParser()->urlFor('core.auth.jwtsignin')) {
                     $en = $this->crypto->encrypt($request->getUri()->getPath(), $this->settings['crypto']['reqparams']);
                     return $handler->handle($request)->withRedirect($this->routecollector->getRouteParser()->urlFor('core.auth.jwtsignin') .'?'. http_build_query(['caller' => $en]));
                 }
             }
+
+        
+
         return $handler->handle($request);
     }
 }
