216aa7797b0a865a1e7861f0ca45d8ead9a12f79
Pavel Stratil <pavel@vaizard.org>
2021-08-25 20:56:43 +0000

    pre-db-write

--- glued/Core/Middleware/AuthorizationMiddleware.php
+++ glued/Core/Middleware/AuthorizationMiddleware.php
@@ -51,8 +51,8 @@ final class AuthorizationMiddleware extends AbstractMiddleware implements Middle
         if (!$hit) {
             $json = $this->utils->fetch_uri($oidc['uri']['discovery']);
             $conf = (array) json_decode($json);
-            if ($conf['issuer'] != $oidc['uri']['realm']) throw new \Exception('Identity backend configuration mismatch');
-            $this->fscache->set('glued_oidc_uri_discovery', $json, 300); // 5 minutes
+            if ($conf['issuer'] != $oidc['uri']['realm']) throw new \Exception('Identity backend configuration mismatch.');
+            $this->fscache->set('glued_oidc_uri_discovery', $json, 300); // TODO make the 300s value configurable
         }
 
         $hit = $this->fscache->has('glued_oidc_uri_jwks');
@@ -64,8 +64,8 @@ final class AuthorizationMiddleware extends AbstractMiddleware implements Middle
         if (!$hit) {
             $json = $this->utils->fetch_uri($oidc['uri']['jwks']);
             $jwks = (array) json_decode($json);
-            if (!isset($jwks['keys'])) throw new \Exception('Identity backend certs mismatch');
-            $this->fscache->set('glued_oidc_uri_jwks', $json, 300); // 5 minutes
+            if (!isset($jwks['keys'])) throw new \Exception('Identity backend certs mismatch.');
+            $this->fscache->set('glued_oidc_uri_jwks', $json, 300); // TODO make the 300s value configurable
         }
 
         $certs = [];
@@ -79,12 +79,15 @@ final class AuthorizationMiddleware extends AbstractMiddleware implements Middle
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
+        // Get oidc config, jwk signing keys and the access token
         $oidc = $this->settings['oidc'];
         $certs = $this->get_jwks($oidc);
         $accesstoken = $_COOKIE['AccessToken'] ?? '';
 
+        // Authenticate user. Exceptions (i.e. invalid jwt, database 
+        // errors etc.) are handled by the catch below.
         try {
-
+            
             $jwt = Load::jws($accesstoken)   // Load and verify the token in $accesstoken
                 ->algs(['RS256', 'RS512'])   // Check if allowed The algorithms are used
                 ->exp()                      // Check if "exp" claim is present
@@ -93,43 +96,51 @@ final class AuthorizationMiddleware extends AbstractMiddleware implements Middle
                 ->iss($oidc['uri']['realm']) // Check if "nbf" claim is present and matches the realm
                 ->keyset(new JWKSet($certs)) // Key used to verify the signature
                 ->run();                     // Do it.
-            $claims = $jwt->claims->all() ?? [];
 
-            $this->view->getEnvironment()->addGlobal('certs', json_decode(json_encode($certs), true));
-            $this->view->getEnvironment()->addGlobal('ahdr', $accesstoken);
-            $this->view->getEnvironment()->addGlobal('jwt_claims', $jwt->claims->all() ?? []);
-            $this->view->getEnvironment()->addGlobal('jwt_header', $jwt->header->all() ?? []);        
-
-            
             $filter = [ 'sub', 'email', 'preferred_username', 'website', 'groups', 'given_name', 'family_name', 'name', 'locale' ];
             $claims = array_intersect_key($jwt->claims->all() ?? [], array_flip($filter)); 
 
             // TODO join with events table
             $this->db->where('c_uuid = uuid_to_bin(?, true)', [ $claims['sub'] ?? '' ]);
-            $t_core_users = $this->db->get('t_core_users', null);
-            
-
-
-            //print_r($data); die();
-
-            //$shadow_json = json_encode($shadow);
-            //$shadow_hash = md5($shadow_json);
-
-            //$profile['email'][] = $shadow['email'];
-            // profi
-            // TODO store data somewhere
-            // TODO shadow profile with validity
-/*
-            if ($claims['sub']) $data["c_uuid"] = $this->db->func('uuid_to_bin(?, true)', [$claims['sub']])
+            $t_core_users = $this->db->getOne('t_core_users', null);
+            if ($t_core_users) {
+                // TODO join the query above with a table containing scheduled events,
+                // periodically update profile data according to identity server.
+            } else {
+                $profile['kind']['natural'] = 1;
+                $profile['name'][0]['fn'] = $claims['name'];
+                $profile['name'][0]['given'] = $claims['given_name'];
+                $profile['name'][0]['family'] = $claims['family_name'];
+                $profile['name'][0]['@']['src'] = $oidc['uri']['realm'];
+                $profile['email'][0]['uri'] = $claims['email'];
+                $profile['email'][0]['@']['src'] = $oidc['uri']['realm'];
+                $profile['email'][0]['@']['pref'] = 1;
+                $profile['service'][0]['kind']   = 'oidc';
+                $profile['service'][0]['uri']    = $oidc['uri']['realm'];
+                $profile['service'][0]['handle'] = $claims['preferred_username'];
+                $profile['website'][0]['uri']    = $claims['website'];
+                $account['locale'] = $claims['locale'];
+                // log do shadow profile log table
+                // a udelat zapis do profile a accoiunts tables
+                // TODO create profile
+                // TODO shadow profile
+            if ($claims['sub']) $data["c_uuid"] = $this->db->func('uuid_to_bin(?, true)', [$claims['sub']]);
             if ($claims['email']) $data["c_email"] = $claims['email'];
             if ($claims['preferred_username']) $data["c_nick"] = $claims['preferred_username'];
             if ($claims['email']) $data["c_email"] = $claims['email'];
 
-            ];
-            if ($this->db->insert('t_core_users', $data)) $respond = 'Welcome';
-            else $respond = 'Hello again';
-            $this->view->getEnvironment()->addGlobal('core_users', $respond);        
-*/
+            
+            //if ($this->db->insert('t_core_users', $data)) $respond = 'Welcome';
+            //else $respond = 'Hello again';
+            //$this->view->getEnvironment()->addGlobal('core_users', $respond);        
+
+            }
+            
+            // Pass jwt data to twig
+            $this->view->getEnvironment()->addGlobal('jwt_claims', $jwt->claims->all() ?? []);
+            $this->view->getEnvironment()->addGlobal('jwt_header', $jwt->header->all() ?? []);        
+
+
 /*
     "name": "Pavel Stratl",
     "groups": [
@@ -150,10 +161,12 @@ final class AuthorizationMiddleware extends AbstractMiddleware implements Middle
 
 
         } catch (\Exception $e) {
+                // Jwt exception
                 if ($request->getUri()->getPath() != $this->routecollector->getRouteParser()->urlFor('core.auth.jwtsignin')) {
                     $en = $this->crypto->encrypt($request->getUri()->getPath(), $this->settings['crypto']['reqparams']);
                     return $handler->handle($request)->withRedirect($this->routecollector->getRouteParser()->urlFor('core.auth.jwtsignin') .'?'. http_build_query(['caller' => $en]));
                 }
+                // TODO Sql exception handling
             }
 
         //if (isset($shadow['sub'])) $request = $request->withAttribute('auth-sub', $shadow['sub']);
