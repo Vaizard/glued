f2b2af499e8e817f3cf6a59008ff4f4427cc2e37
Pavel Stratil <pavel@vaizard.org>
2021-10-07 22:20:03 +0000

    correct contacts routes

--- glued/Contacts/routes.php
+++ glued/Contacts/routes.php
@@ -10,14 +10,14 @@ use Slim\Routing\RouteCollectorProxy;
 $app->group('/api/contacts/v1', function (RouteCollectorProxy $group) {
     $group->get ('[/{uid:[0-9]+}]', ContactsController::class . ':contacts_get_api')->setName('api.contacts.v1'); 
     $group->post('[/{uid:[0-9]+}]', ContactsController::class . ':contacts_post_api');
-})->add(RestrictGuests::class);
+});
 
 $app->group('/contacts', function (RouteCollectorProxy $group) {
     $group->get ('/items[/{uid:[0-9]+}]', ContactsController::class . ':contacts_get_app')->setName('app.contacts'); 
-})->add(RedirectGuests::class);
+});
 
 $app->group('/api/if/regs/v1', function (RouteCollectorProxy $group) {
     $group->get ('/cz/names[/{name}]', ContactsController::class . ':cz_names')->setName('api.if.regs.cz.orgname.v1'); 
     $group->get ('/cz/ids[/{id}]', ContactsController::class . ':cz_ids')->setName('api.if.regs.cz.orgid.v1'); 
     $group->get ('/eu/ids[/{id}]', ContactsController::class . ':eu_ids')->setName('api.if.regs.eu.orgid.v1'); 
-})->add(RestrictGuests::class);
+});
